506de0251080e27333936af5ee60e95a300bfb1a
sunluelectric <sunlu.electric@gmail.com>
2021-02-10 14:03:33 +0800

    Add lesson103.R

---
+++ lesson103.R
@@ -0,0 +1,30 @@
+### File: lesson103.R
+
+## Part 1. Visualize linear regression
+
+# Prepare data
+library(datasets)
+# Good habit to first visualize the data
+plot(mtcars$wt,mtcars$mpg,
+     pch = 19,
+     col = "red",
+     main = "MPG as a Funciton of Cars Weights",
+     xlab = "Weight",
+     ylab = "Miles per Gallon"
+     )
+
+## Part 2. Superimpose plot
+
+# Prepare data
+hist(lynx,
+     breaks = 14,
+     freq = FALSE, # axis shows density (sample number / total number) instead of frequency (sample number)
+     col = "thistle1"
+     )
+
+# To be superimposed
+curve(dnorm(x,mean=mean(lynx),sd=sd(lynx)),
+      col = "thistle4",
+      lwd = 2, # line width
+      add = TRUE # superimpose to the previous figure
+      )
